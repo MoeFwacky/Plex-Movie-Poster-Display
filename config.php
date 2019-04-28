a5fce712cfc25b6b896964b14d8dd2aa8a697fe3
Moe Fwacky <moe.fwacky@gmail.com>
2019-04-28 09:17:17 -0700

    Add files via upload

--- config.php
+++ config.php
@@ -1,20 +1,50 @@
-
-
 <?php
-//Server Configuration
-$plexServer = '192.168.1.140';
-$plexport = '32400';
-$plexToken = 'z9HsyiDhUxp8rkUQsrEP';
+include('./psConfig.php');
+$get_plex_token = array();
+$plexServer_url = array();
+$getConfig = array();
+$clientsxml = array();
+$clientdata = array();
+//Pseudo Channel Config File
+$pseudoConfig = $pseudochannel . "pseudo_config.py";
+$getConfig = file_get_contents($pseudoConfig);
+$getConfig = explode("\n",$getConfig);
+
+//Get Plex data from plex_token file in Pseudo Channel
+$plex_token = $pseudochannel . "plex_token.py";
+$get_token = file_get_contents($plex_token);
+$get_plex_token = explode("\n", $get_token);
+$baseurl = $get_plex_token['1'];
+$baseurl = str_replace("'","",$baseurl);
+$baseurl = str_replace("baseurl = ","",$baseurl);
+$plexServer_url = parse_url($baseurl);
+$plexServer = $plexServer_url['host'];
+$plexport = $plexServer_url['port'];
+$token = $get_plex_token['0'];
+$token = str_replace("'","",$token);
+$plexToken = str_replace("token = ","",$token);
+
+//Get Client Name from Pseudo Channel Config
+if (isset($_GET['tv'])) {
+	$plexClientName = $_GET['tv'];
+} else {
+	$plexClientName = $getConfig[40];
+	$plexClientName = trim($plexClientName, 'plexClients = ["');
+	$plexClientName = str_replace('"]','',$plexClientName);
+	$plexClientName = trim($plexClientName);
+}
 
 
-//Cleint Configuration
-$plexClient = '192.168.1.102';
-
-
-//Pseudo Channel
-$pseudochannel = '/home/pi/channels/';
-
-
-//Display Type
-$DisplayType = 'full';
+//Get Other Plex Client Data from Plex API
+$clientsurl = "http://" . $plexServer . ":" . $plexport . "/clients?X-Plex-Token=" . $plexToken;
+$getclientsxml = file_get_contents($clientsurl);
+$clientsxml = simplexml_load_string($getclientsxml);
+foreach($clientsxml->Server as $key => $clientdata) {
+		$clientname = $clientdata[name];
+		if($clientname == $plexClientName) {
+			$plexClientIP = $clientdata[address];
+			$plexClient = trim($plexClientIP);
+			$plexClientUID = $clientdata[machineIdentifier];
+		}
+}
 ?>
--- getData.php
+++ getData.php
@@ -1,8 +1,11 @@
 <?php
-include 'config.php'; #Get variables from config
+include 'config.php'; //Get variables from config
 $results = Array();
+if (isset($_GET['tv'])) {
+        $plexClientName = $_GET['tv'];
+}
 
-# GET PLEX DATA
+//GET PLEX DATA
 $url = "http://" . $plexServer . ":" . $plexport . "/status/sessions?X-Plex-Token=" . $plexToken; #set plex server url
 $getxml = file_get_contents($url);
 $xml = simplexml_load_string($getxml) or die("feed not loading");
@@ -11,29 +14,42 @@ $time_style=NULL;
 $top_line=NULL;
 $middle_line=NULL;
 $bottom_line=NULL;
+$dircontents=array();
+$nowplaying=array();
+$xmldata=array();
+$chantable=array();
+$ps_boxes=array();
 
-# SET TIME AND DATE
-$date = date('H:i'); #get current time
-$day = date('D F d'); #get current date
+//SET TIME AND DATE
+$rightnow = time(); //time
+$date = date('H:i'); //also time
+$day = date('D F d'); //date
 $text_color='cyan';
 $text_color_alt='cyan';
 
-#CHECK IF PSEUDO CHANNEL IS RUNNING AND ON WHAT CHANNEL
+//CHECK IF PSEUDO CHANNEL IS RUNNING AND ON WHAT CHANNEL
 $is_ps_running = "find " . $pseudochannel . " -name running.pid -type f -exec cat {} +";
 $ps_channel_id = "find " . $pseudochannel . " -name running.pid -type f";
-$pgrep = shell_exec($is_ps_running); #check if pseudo channel is running
-$pdir = shell_exec($ps_channel_id); #identify which channel is running
-$channel_num = str_replace($pseudochannel . "pseudo-channel_", "", $pdir);
-$channel_num = ltrim($channel_num, '0');
-$channel_num = str_replace("/running.pid", "", $channel_num);
+$pgrep = shell_exec($is_ps_running); //check if pseudo channel is running
+$pdir = shell_exec($ps_channel_id); //identify directory has the running.pid file
+$channel_number = str_replace($pseudochannel . "pseudo-channel_", "", $pdir); //strip the prefix of the directory name to get the channel number
+$channel_num = ltrim($channel_number, '0'); //strip the leading zero from single digit channel numbers
+$channel_num = str_replace("/running.pid", "", $channel_num); //strip running.pid filename from the variable
+$chnum = str_replace("/running.pid", "",$channel_number); //strip running.pid from the variable that keeps the leading zero
+$chnum = trim($chnum);
 
-# LINE STYLE VARIABLES
+//GET ALL PSEUDO CHANNEL DAILY SCHEDULE XML FILE LOCATIONS
+$lsgrep = exec("find ". $pseudochannel . "pseudo-channel_*/schedules | grep xml | tr '\n' ','"); //list the paths of all daily schedule xml files in a comma separated list
+$dircontents = explode(",", $lsgrep); //write file locations into an array
+$ps_search = rtrim($pseudochannel, '/'); //strip trailing slash from pseudo channel root folder to enable search for other client setups
+
+// LINE STYLE VARIABLES
 if ($DisplayType == 'half') {
-	$time_style = "<p style='font-family: Digital\-7; font-size: 95px; font-style: monospace; text-shadow: 1px 1px 3px cyan, 0 0 25px cyan, 0 0 5px black; color: cyan; line-height: 92%; text-align: left; z-index: 100; top: 0px; position: absolute; width: 350px; left: 140px; margin-left: 0px;'>";
-	$top_line = "<p style='font-family: Digital\-7; font-size: 35px; text-shadow: 5px 5px 10px black, 0 0 25px black, 0 0 5px black; color: cyan; line-height: 92%; text-align: left; z-index: 100; top: 80px; position: absolute; white-space: nowrap; overflow: hidden; width: 480px; left: 140px; margin-left: 0px;'>";
-	$middle_line = "<p style='font-family: Digital\-7; font-size: 35px; text-align: left; text-shadow: 5px 5px 10px black, 0 0 25px black, 0 0 5px black; z-index: 100; top: 100px; color: cyan; left: 140px; position: absolute; width: 350px; margin-left: 0px;'>";
-	$bottom_line = "<p style='font-family: Digital\-7; font-size: 35px; text-align: left; text-shadow: 5px 5px 10px black, 0 0 25px black, 0 0 5px black; z-index: 100; color: cyan; line-height: 90%; top: 140px; left: 140px; position: absolute; width: 350px; margin-left: 0px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 480px;'>";
-	$side_channel = "<p style='font-family: Digital\-7; font-size: 20px; text-shadow: 5px 5px 10px black, 0 0 25px black, 0 0 5px black; color: cyan; line-height: 92%; text-align: right; z-index: 100; top: 10px; position: absolute; width: 480px; right: 27px; margin-left: 0px;'>Channel $channel_num</p>";
+	$time_style = "<p class='vcr-time-half'>";
+	$top_line = "<p class='vcr-info-half-1'>";
+	$middle_line = "<p class='vcr-info-half-2'>";
+	$bottom_line = "<p class='vcr-info-half-3'>";
+	$side_channel = "<p class='vcr-side-half'>Channel $channel_num</p>";
 
 	$position_half = "<img position: absolute; align: top; width='480' style='opacity:1;'>";
 }
@@ -43,14 +59,14 @@ if ($DisplayType == 'full') {
           if(strstr($playdata->Player['address'], $plexClient)) {
 			$video_duration = (int)$playdata['duration'];
 			if($playdata['type'] == "movie") {
-				if ($video_duration < "1800000") { #COMMERCIAL
+				if ($video_duration < "1800000") { //COMMERCIAL
 				$text_color='cyan';
 				$text_color_alt='cyan';
-				} else { #MOVIE
+				} else { //MOVIE
 				$text_color='yellow';
 				$text_color_alt='white';
 				}
-			} elseif($playdata['type'] == "show" || $playdata['parentTitle'] != "") { #SHOW
+			} elseif($playdata['type'] == "show" || $playdata['parentTitle'] != "") { //TV SHOW
 			$text_color='yellow';
 			$text_color_alt='white';
 			} else {
@@ -61,11 +77,11 @@ if ($DisplayType == 'full') {
 		  }
 
 # SET FULL OPTIONS
-	$time_style = "<p style='font-family: Digital\-7; font-size: 195px; font-style: monospace; text-shadow: 1px 1px 3px black, 0 0 25px black, 0 0 5px black; color:" . $text_color . "; line-height: 92%; text-align: center; z-index: 100; top: 30px; position: absolute; width: 480px; left: 50%; margin-left: -230px;'>";
-	$top_line = "<p style='font-family: Digital\-7; font-size: 45px; color: $text_color; text-shadow: 5px 5px 10px black, 0 0 25px black, 0 0 5px black; line-height: 92%; text-align: center; z-index: 100; position: absolute; top: 200px; width: 480px; left: 50%; margin-left: -240px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis ,   ; max-width: 480px;'>";
-	$middle_line = "<p style='font-family: Digital\-7; font-size: 35px; text-align: center; text-shadow: 5px 5px 10px black, 0 0 25px black, 0 0 5px black; z-index: 100; top: 230px; color: $text_color_alt; position: absolute; width: 480px; left: 50%; margin-left: -240px;'>";
-	$bottom_line = "<p style='font-family: Digital\-7; font-size: 35px; text-align: center; text-shadow: 5px 5px 10px black, 0 0 25px black, 0 0 5px black; z-index: 100; color: $text_color; line-height: 90%; top: 275px; position: absolute; width: 480px; left: 50%; margin-left: -240px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 480px;'>";
-	$side_channel = "<p style='font-family: Digital\-7; font-size: 30px; text-shadow: 5px 5px 10px black, 0 0 25px black, 0 0 5px black; color: $text_color_alt; line-height: 92%; text-align: center; z-index: 100; top: 25px; position: absolute; width: 480px; left: 50%; margin-left: -240px;'>Channel $channel_num</p>";
+	$time_style = "<p class='vcr-time-full-idle' style='color: $text_color'>";
+	$top_line = "<p class='vcr-info-full-1-idle' style=color: $text_color'>";
+	$middle_line = "<p class='vcr-info-full-2-idle' style=color: $text_color_alt'>";
+	$bottom_line = "<p class='vcr-info-full-3-idle' style=color: $text_color'>";
+	$side_channel = "<p class='vcr-side-full' style='color: $text_color_alt'>Channel $channel_num</p>";
 
 	$position_play_full = "<img position: absolute; top: 20px; width='480' style='opacity:1;'>";
 	$position_idle_full = "<img position: absolute; top: 0; src='/assets/vcr-play.jpg' width='480' style='opacity:1;'>";
@@ -75,7 +91,7 @@ if(strcmp($channel_num," ")<=0){
 	$channel_num=0;
 }
 
-#If Nothing is Playing
+//If Nothing is Playing
 $text_color='cyan';
 $text_color_alt='cyan';
 if ($DisplayType == 'full') {
@@ -84,20 +100,22 @@ if ($DisplayType == 'full') {
 if ($DisplayType == 'half') {
 	$position=$position_half;
 }
-if ($pgrep >= 1) { #PSEUDO CHANNEL ON
+if ($pgrep >= 1) { //PSEUDO CHANNEL ON
 	$top_section = $time_style . $date . "</p>" . $position;
 	$middle_section = $top_line . "Channel $channel_num</p>";
 	$bottom_section = $middle_line . "</p>";
-} else { #PSEUDO CHANNEL OFF
+	$nowplaying = "Channel $channel_num Standing By...";
+} else { //PSEUDO CHANNEL OFF
 	$top_section = $time_style . $date . "</p>" . $position;
 	$middle_section = $top_line . $day . "</p>";
 	$bottom_section = "<p></p>";
+	$nowplaying = "Please Stand By...";
 }
 
-  if ($xml['size'] != '0') { # IF PLAYING CONTENT
+  if ($xml['size'] != '0') { //IF PLAYING CONTENT
       foreach ($xml->Video as $clients) {
-          if(strstr($clients->Player['address'], $plexClient)) {
-			    #IF PLAYING COMMERCIAL
+          if(strstr($clients->Player['address'], $plexClient)) { //If the active client on plex matches the client in the config
+			    //IF PLAYING COMMERCIAL
 				if($clients['type'] == "movie" && $clients['duration'] < 1800000) {
 	          			#$text_color='cyan';
 					#$text_color_alt='cyan';
@@ -110,8 +128,10 @@ if ($pgrep >= 1) { #PSEUDO CHANNEL ON
 					$top_section = $time_style . $date . "</p>" . $position;
 					$middle_section = $top_line . $clients['librarySectionTitle'] . "</p>";
 					$bottom_section = "<p></p>";
+					$title_clean = str_replace("_", " ", $clients['title']);
+					$nowplaying = "<a href='schedule.php' style='color:white'>Now Playing: " . $title_clean . " on Channel ". $channel_num . "</a>";
 				}
-				#IF PLAYING MOVIE
+				//IF PLAYING MOVIE
 				if($clients['type'] == "movie" && $clients['duration'] >= 1800000) {
 					$text_color='yellow';
 					$text_color_alt='white';
@@ -129,8 +149,9 @@ if ($pgrep >= 1) { #PSEUDO CHANNEL ON
 					$top_section = $background_art . $time_style . $date . $side_channel . "</p>" . $position;
 					$middle_section = $top_line . $clients['title'] . $middle_line . $clients['year'] . "</p>";
 					$bottom_section = $bottom_line . $clients['tagline'] . "</p>";
+					$nowplaying = "<a href='schedule.php' style='color:white'>Now Playing: " . $clients['title'] . " (" . $clients['year'] . ")" . " on Channel ". $channel_num . "</a>";
 				}
-				#IF PLAYING TV SHOW
+				//IF PLAYING TV SHOW
 				if($clients['type'] == "show" || $clients['parentTitle'] != "") {
 					if ($DisplayType == 'half') {
 						$art = $clients['parentThumb'];
@@ -147,13 +168,79 @@ if ($pgrep >= 1) { #PSEUDO CHANNEL ON
 					$top_section =  $background_art . $time_style . $date . "</p>" . $position;
 					$middle_section = $top_line . $clients['grandparentTitle'] . "</p>" . $middle_line . $clients['parentTitle'] . ", Episode " . $clients['index'] . "</p>";
 					$bottom_section = $bottom_line . $clients['title'] . "</p>" . $side_channel . "</p>";
+					$nowplaying = "<a href='schedule.php' style='color:white'>Now Playing: " . $clients['grandparentTitle'] . " • " . $clients['parentTitle'] . ", Episode " . $clients['index'] . " • " . $clients['title'] . " on Channel ". $channel_num . "</a>";
 					}
 				}
 		  }
 	  }
 
+//BUILD DAILY SCHEDULE PAGES
+$doheader = "0";
+foreach ($dircontents as $xmlfile) { //do the following for each xml schedule file
+	$xmldata = simplexml_load_file($xmlfile); //load the xml schedule file
+	foreach($xmldata->time as $attributes) { //for each entry in the schedule, do the following
+		$start_time_unix = strtotime($attributes['time-start']); //get the entry start time
+	    	$start_time_human = date("H:i", $start_time_unix); //convert start time to readable format
+		$duration_seconds = $attributes['duration']/1000; //get entry duration and convert to seconds
+		$duration_seconds = $duration_seconds-1;
+		$end_time_unix = $start_time_unix + $duration_seconds; //using start time and duration, calculate the end time
+		$end_time_human = date("H:i", $end_time_unix); //end time in readable format
+		$ch_file = str_replace($pseudochannel . "pseudo-channel_", "ch", $xmlfile); //get channel number
+		$ch_file = str_replace("/schedules/pseudo_schedule.xml", "", $ch_file);
+		$ch_number = str_replace("ch", "", $ch_file);
+		if ($doheader != "1") {
+			$tableheader = "<table class='schedule-table'><tr><th>&nbsp;Channel&nbsp;</th><th>Time</th><th>Title</th></tr>";
+			$chantableheader = "<table class='schedule-table'><tr><th colspan='2'>";
+			$nowtable = $tableheader;
+			$doheader = "1";
+		}
+		if ($chnum == $ch_number) {
+			$channelplaying = "font-weight:bold;font-size:1.1em";
+		} else {
+			$channelplaying = "";
+		}
+		if ($rightnow >= $start_time_unix && $rightnow <= $end_time_unix) {
+			$nowtable .= "<tr><td><a style='$channelplaying;display:block; width:100%' href='schedule.php?ch=$ch_number'>" . $ch_number . "</a></td>";
+			$nowtable .= "<td style='$channelplaying'>" . $start_time_human . " - " . $end_time_human . " </td>";
+			$nowtable .= "<td style='$channelplaying;text-align:left'><a style='display:block;width:100%' href='?action=channel&num=$ch_number'>&nbsp";
+			if ($attributes['type'] == "TV Shows") {
+				$nowtable .= $attributes['show-title'];
+				$nowtable .= "</br>&nbsp;S" . $attributes['show-season'] . "E" . $attributes['show-episode'] . " - " . $attributes['title'] . "</td>";
+			} elseif ($attributes['type'] == "Commercials") {
+				$nowtable .= $attributes['type'] . "</td>";
+			} else {
+				$nowtable .= $attributes['title'] . "</a></td>";
+			}
+		}
+		if ($results[$ch_file] == "") {
+			$results[$ch_file] = $chantableheader . "<a href='schedule.php?action=channel&num=$ch_number'>Channel " . $ch_number . "</a></th></tr><th>Time</th><th>Title</th></tr></tr>";
+		}
+		if ($rightnow >= $start_time_unix && $rightnow < $end_time_unix) {
+			$isnowplaying = "font-weight:bold;font-size:1.2em";
+		} else {
+			$isnowplaying = "";
+		}
+		if ($attributes['type'] != "Commercials") {
+			$results[$ch_file] .= "<tr>";
+			$results[$ch_file] .= "<td style='$isnowplaying'>" . $start_time_human . " - " . $end_time_human . " </td>";
+			$results[$ch_file] .= "<td style='$isnowplaying;text-align:left'>&nbsp;";
+			if ($attributes['type'] == "TV Shows") {
+				$results[$ch_file] .= $attributes['show-title'];
+				$results[$ch_file] .= "</br>&nbsp;S" . $attributes['show-season'] . "E" . $attributes['show-episode'] . " - " . $attributes['title'] . "</td>";
+			} elseif ($attributes['type'] == "Commercials") {
+				$results[$ch_file] .= $attributes['type'] . "</td>";
+			} else {
+				$results[$ch_file] .= $attributes['title'] . "</td>";
+			}
+		}
+	}
+}
+$nowtable .= "</table>";
+$results[$ch_file] .= "</table>";
+$results['rightnow'] = $nowtable;
 $results['top'] = "$top_section";
 $results['middle'] = "$middle_section $bottom_section";
 $results['bottom'] = "<p></p>";
+$results['nowplaying'] = "$nowplaying";
 echo json_encode($results);
 ?>
