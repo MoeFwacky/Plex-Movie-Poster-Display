09da386d4ede5f62d712abe216acd533da96de96
Moe Fwacky <moe.fwacky@gmail.com>
2020-07-06 16:59:29 -0700

    Update install_dependencies.sh
    added dependency needed for recent update

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -8,5 +8,5 @@
 #
 # Then apache2 is reset to be careful
 sudo apt-get update
-sudo apt-get install php apache2 libapache2-mod-php php-xml
+sudo apt-get install php apache2 libapache2-mod-php php-xml php-sqlite3
 sudo service apache2 restart
