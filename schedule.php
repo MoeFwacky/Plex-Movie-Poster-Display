a5db1f477bbf08ff81840f8f6649a8910cf59870
Moe Fwacky <moe.fwacky@gmail.com>
2020-07-06 16:53:12 -0700

    Add files via upload

--- schedule.php
+++ schedule.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <?php
-session_start();
 $ch_file="";
 include('./control.php');
 include('./config.php');
@@ -18,6 +17,53 @@ foreach ($tvlocations as $tvbox) {
 ?>
 <html lang="en" class="no-js" style="height:100%">
 	<head>
+            <script
+            src="https://code.jquery.com/jquery-2.2.4.min.js"
+            integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+            crossorigin="anonymous">
+            </script>
+                <script>
+                        const queryString = window.location.search;
+                        const urlParams = new URLSearchParams(queryString);
+                        const tv = urlParams.get('tv');
+			if (tv != "null") {
+				topbar = "topbar.php?tv="+tv;
+                        	getdata = "getData.php?tv="+tv;
+				getclock = "getClock.php?tv="+tv;
+			} else {
+				topbar = "topbar.php";
+				getdata = "getData.php";
+				getclock = "getClock.php";
+			}
+                        $(document).ready( function() {
+                                $("#topbar").load(topbar);
+				$.getJSON(getdata,function(data) {
+					$.each(data, function(key,val) {
+						$('#'+key).html(val);
+					});
+				});
+                        });
+                        $(document).ready(
+                            function() {
+                                setInterval(function() {
+                                    $.getJSON(getdata,function(data) {
+                                        $.each(data, function(key, val) {
+                                            $('#'+key).html(val);
+                                        });
+                                    });
+                                }, 60000);
+                            });
+			$(document).ready(
+				function() {
+					setInterval(function() {
+						$.getJSON(getclock,function(data) {
+							$.each(data, function(key, val) {
+								$('#'+key).html(val);
+							});
+						});
+					}, 5000);
+				});
+                </script>
 		<style type="text/css">a {text-decoration: none}</style>
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -49,33 +95,7 @@ foreach ($tvlocations as $tvbox) {
 			}
 		}
 		</script>
-	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	    <script>
-		$(document).ready( function() {
-			$("#topbar").load("topbar.php");
-		});
-	    </script>
-                <script
-            src="https://code.jquery.com/jquery-2.2.4.min.js"
-            integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
-            crossorigin="anonymous">
-            </script>
-		<script>
-		        $(document).ready(
-		            function() {
-		                setInterval(function() {
-		                    $.getJSON('getData.php',function(data) {
-		                        $.each(data, function(key, val) {
-		                            $('#'+key).html(val);
-		                        });
-		                    });
-		                }, 5000);
-		            });
-		</script>
 		<script language="JavaScript">
-		function channel() {
-			<?php $id="$ch_file"; ?>
-		}
 		function httpGet(theUrl)
 		{
 			var xmlHttp = new XMLHttpRequest();
@@ -88,31 +108,40 @@ foreach ($tvlocations as $tvbox) {
 	<body>
 		
 		<?php
-		session_start();
 		if (isset($_GET['ch'])) {
 			$id= "ch" . $_GET['ch'];
 		} else {
 			$id="rightnow";
 		}
 		if (isset($_GET['tv'])) {
-			$_SESSION['tv'] = $_GET['tv'];
-			$urlstring = "tv=" . $_GET['tv'] . "&";
-			$_SESSION['urlstring'] = $urlstring;
+			$tv = $_GET['tv'];
+			if ($tv != "null") {
+				$plexClientName = $_GET['tv'];
+				$urlstring = "tv=" . $_GET['tv'] . "&";
+				$urlstring = $urlstring;
+				if ($_GET['tv'] != $configClientName) {
+					$pseudochannel = $pseudochannelTrim . "_" . $_GET['tv'] . "/";
+					$pseudochannel = trim($pseudochannel);
+				}
+			} else {
+				$tv = plexClientName;
+				$urlstring = "";
+			}
 		} else {
-			$_SESSION['tv'] = $plexClientName;
+			$tv = $plexClientName;
 		}
 		if (isset($_GET['time'])) {
-		        $_SESSION['time'] = $_GET['time'];
+		        $time = $_GET['time'];
 	        } else {
-		        $_SESSION['time'] = "0";
+		        $time = "0";
 		}
-		session_write_close();
 
 		$dircontents=array();
 		//GET ALL PSEUDO CHANNEL DAILY SCHEDULE XML FILE LOCATIONS
-		$lsgrep = exec("find ". $pseudochannelMaster . "pseudo-channel_*/schedules | grep xml | tr '\n' ','"); //list the paths of all daily schedule xml files in a comma separ$
+		$lsgrep = exec("find ". $pseudochannelMaster . "pseudo-channel_*/schedules | grep xml | tr '\n' ','"); //list the paths of all daily schedule xml files in as comma sparated
 		$dircontents = explode(",", $lsgrep); //write file locations into an array
-		$scheduleTable = "<table width='100%' max-width='100%' class='schedule-table'><thead><tr width='100%'><th width='4%' max-width='4%'>&nbsp;Ch.&nbsp;</th><th colspan='1' width='8%' max-width='8%' id=nowTime>Now</th><th colspan='1' width='8%' max-width='8%' id=timePlus15>+15</th><th colspan='1' width='8%' max-width='8%' id=timePlus30>+30</th><th colspan='1' width='8%' max-width='8%' id=timePlus45>+45</th><th colspan='1' max-width='8%' width='8%' id=timePlus60>+60</th><th colspan='1' width='8%' max-width='8%' id=timePlus75>+75</th><th colspan='1' max-width='8%' width='8%' id=timePlus90>+90</th><th colspan='1' width='8%' max-width='8%' id=timePlus105>+105</th><th colspan='1' max-width='8%' width='8%' id=timePlus120>+120</th><th colspan='1' width='8%' max-width='8%' id=timePlus135>+135</th><th colspan='1' max-width='8%' width='8%' id=timePlus150>+150</th><th colspan='1' width='8%' max-width='8%' id=timePlus165>+165</th></tr></thead><tbody>";
+		$scheduleTable = "<table width='100%' max-width='100%' class='schedule-table'><thead><tr width='100%'><th width='4%' max-width='4%'>&nbsp;Ch.&nbsp;</th><th colspan='1' width='8%' max-width='8%' id=nowtime>Now</th><th colspan='1' width='8%' max-width='8%' id=timePlus15>+15</th><th colspan='1' width='8%' max-width='8%' id=timePlus30>+30</th><th colspan='1' width='8%' max-width='8%' id=timePlus45>+45</th><th colspan='1' max-width='8%' width='8%' id=timePlus60>+60</th><th colspan='1' width='8%' max-width='8%' id=timePlus75>+75</th><th colspan='1' max-width='8%' width='8%' id=timePlus90>+90</th><th colspan='1' width='8%' max-width='8%' id=timePlus105>+105</th><th colspan='1' max-width='8%' width='8%' id=timePlus120>+120</th><th colspan='1' width='8%' max-width='8%' id=timePlus135>+135</th><th colspan='1' max-width='8%' width='8%' id=timePlus150>+150</th><th colspan='1' width='8%' max-width='8%' id=timePlus165>+165</th></tr></thead><tbody>";
+		$nowPlayingDisplay = "<p style='color:yellow'>$plexClientName <span style='color:white' id='nowplaying' class='container'>Please Stand By</span></p>";
 
 		foreach ($dircontents as $xmlfile) { //do the following for each xml schedule file
 		$ch_file = str_replace($pseudochannelMaster . "pseudo-channel_", "ch", $xmlfile); //get channel number
@@ -120,30 +149,18 @@ foreach ($tvlocations as $tvbox) {
 		$ch_number = str_replace("ch", "", $ch_file);
 		$ch_row = "row" . $ch_number;
 		$ch_cell = "chan" . $ch_number;
-		$nowtime_cell = $ch_number . "_nowTime";
-		$plus15_cell = $ch_number . "_plus15";
-		$plus30_cell = $ch_number . "_plus30";
-		$plus45_cell = $ch_number . "_plus45";
-		$plus60_cell = $ch_number . "_plus60";
-		$plus75_cell = $ch_number . "_plus75";
-		$plus90_cell = $ch_number . "_plus90";
-		$plus105_cell = $ch_number . "_plus105";
-		$plus120_cell = $ch_number . "_plus120";
-		$plus135_cell = $ch_number . "_plus135";
-		$plus150_cell = $ch_number . "_plus150";
-		$plus175_cell = $ch_number . "_plus175";
-				if($xmlfile){
-						$xmldata = simplexml_load_file($xmlfile); //load the xml schedule file
-				}
+		if($xmlfile){
+			$xmldata = simplexml_load_file($xmlfile); //load the xml schedule file
+		}
 		if($xmldata){
-		$scheduleTable .= "<tr class='schedule-table' max-width='100%' min-width='100%' width='100%' id='$ch_row'></tr>";
+			$scheduleTable .= "<tr class='schedule-table' max-width='100%' min-width='100%' width='100%' id='$ch_row'></tr>";
 		}
 		}
-		$scheduleTable .= "</tbody></table>"
+		$scheduleTable .= "</tbody></table>";
 		?>
 		<div class="container main-container">
-			<div class="container" style="" scrolling="no"><span style="color:yellow"><?php echo $plexClientName; ?> <span style="color:white" id="nowplaying" class="container">Please Stand By</span></div>
-			<div id="schedule" class="container" name="schedule"><?php echo $scheduleTable; ?></div>
+			<?php echo $nowPlayingDisplay;
+			echo $scheduleTable; ?>
 			</br><a href="schedule.php?action=updateweb&<?php echo $urlstring; ?>" style="color:white" href="">Update Channel Schedule Data &#8594;</a>
 		</div><!-- /container -->
 		<div id="topbar" name="topbar"></div>
